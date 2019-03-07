d27434b7cb97ccf1fe370e6085d49104bf4c07e3
Luke Hengstenberg <lukehengstenberg@gmail.com>
2019-03-07 16:27:18 +0000

    Added controller to display list of current users

---
+++ TheMusicExchangeProject/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheMusicExchangeProject.Models;
+
+namespace TheMusicExchangeProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly TheMusicExchangeProjectContext _context;
+
+        public AccountController(TheMusicExchangeProjectContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+    }
+}
