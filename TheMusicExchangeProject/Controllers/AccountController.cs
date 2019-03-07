6989b99cf470bfa5c6a958d41445745aceec1d4d
Luke Hengstenberg <lukehengstenberg@gmail.com>
2019-03-07 17:23:42 +0000

    Displays Skills and Skill levels for each user. Calculated Age.

--- TheMusicExchangeProject/Controllers/AccountController.cs
+++ TheMusicExchangeProject/Controllers/AccountController.cs
@@ -18,8 +18,18 @@ namespace TheMusicExchangeProject.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Users.ToListAsync());
+            var viewModel = from o in _context.Users join o2 in _context.Skills on o.Id equals o2.UserID where o.Id.Equals(o2.UserID) select new UserSkillsViewModel { Users = o, Skills = o2, SkillLevel = o2.Level.Name, Age = CalculateAge(o.DOB)};
+            return View(viewModel);
         }
-
+        private static int CalculateAge(DateTime dateOfBirth)
+        {
+            int age = 0;
+            age = DateTime.Now.Year - dateOfBirth.Year;
+            if(DateTime.Now.DayOfYear < dateOfBirth.DayOfYear)
+            {
+                age = age - 1;
+            }
+            return age;
+        }
     }
 }
---
+++ TheMusicExchangeProject/Models/UserSkillsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheMusicExchangeProject.Areas.Identity.Data;
+
+namespace TheMusicExchangeProject.Models
+{
+    public class UserSkillsViewModel
+    {
+        public TheMusicExchangeProjectUser Users { get; set; }
+        public Skill Skills { get; set; }
+        public string SkillLevel { get; set; }
+        public int Age { get; set; }
+    }
+}
