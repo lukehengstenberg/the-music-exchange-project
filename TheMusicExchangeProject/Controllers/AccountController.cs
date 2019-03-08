3e352858894debe9925d9b7d09d619d025971ba6
Luke Hengstenberg <lukehengstenberg@gmail.com>
2019-03-08 16:35:12 +0000

    Added Filter by Skill Level (fixed bug stopping Skill and Skill Level from not working together)

--- TheMusicExchangeProject/Controllers/AccountController.cs
+++ TheMusicExchangeProject/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TheMusicExchangeProject.Models;
 
@@ -16,9 +17,10 @@ namespace TheMusicExchangeProject.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string searchString, string selectedLevel)
         {
             ViewData["skillFilter"] = searchString;
+            ViewData["levelFilter"] = selectedLevel;
 
             var skills = from s in _context.Skills
                         select s;
@@ -26,7 +28,29 @@ namespace TheMusicExchangeProject.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 skills = skills.Where(s => s.SkillName.Contains(searchString.ToUpper()));
+                if(selectedLevel.Equals("1"))
+                {
+                    skills = skills.Where(s => s.SkillName.Contains(searchString.ToUpper()) && s.Level.Name.Contains("Beginner"));
+                }
+                if (selectedLevel.Equals("2"))
+                {
+                    skills = skills.Where(s => s.SkillName.Contains(searchString.ToUpper()) && s.Level.Name.Contains("Intermediate"));
+                }
+                if (selectedLevel.Equals("3"))
+                {
+                    skills = skills.Where(s => s.SkillName.Contains(searchString.ToUpper()) && s.Level.Name.Contains("Advanced"));
+                }
             }
+
+            ViewBag.levels = new SelectList(new[]
+            {
+                new{ id = "", level = "All"},
+                new{ id = "1", level = "Beginner"},
+                new{ id = "2", level = "Intermediate"},
+                new{ id = "3", level = "Advanced"},
+            },
+            "id", "level", selectedLevel);
+
             var viewModel = from o in _context.Users join o2 in skills on o.Id equals o2.UserID where o.Id.Equals(o2.UserID) select new UserSkillsViewModel { Users = o, Skills = o2, SkillLevel = o2.Level.Name, Age = CalculateAge(o.DOB)};
             return View(viewModel);
         }
